89f38ee50dea1b27fae54e908b2d51758fd1424f
degijanos <janos.degi@auri.hu>
2024-09-24 10:56:09 +0200

    initial commit

---
+++ src/main/java/io/trainticket/TrainTicketBackendApplication.java
@@ -0,0 +1,13 @@
+package io.trainticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainTicketBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainTicketBackendApplication.class, args);
+	}
+
+}
